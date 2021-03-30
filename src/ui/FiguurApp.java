7bed44be9e9f88d441d7975a24488e8d2d2fe920
FilipVanUytvanck <r0830073@ucll.be>
2021-03-30 15:19:21 +0200

    Story 06 - Doener Filip, denker Filip met TODO

--- src/ui/FiguurApp.java
+++ src/ui/FiguurApp.java
@@ -12,21 +12,21 @@ public class FiguurApp {
     public FiguurApp(GridPane root) {
         mogelijkeFiguren = FXCollections.observableArrayList("Cirkel", "Rechthoek", "Lijnstuk", "Driehoek");
         keuzeMenu = new ComboBox(mogelijkeFiguren);
-        root.add(keuzeMenu,0,0);
+        root.add(keuzeMenu, 0, 0);
         keuzeMenu.setOnAction(eventKeuze -> {
-                    keuzeMenu.setVisible(false);
-                    if (keuzeMenu.getValue()!=null) {
-                        if (keuzeMenu.getValue().equals("Cirkel")) {
-                            new CirkelApp(root);
-                        } else if (keuzeMenu.getValue().equals("Rechthoek")) {
-                            new RechthoekApp(root);
-                        } else if (keuzeMenu.getValue().equals("Lijnstuk")) {
-                            new LijnStukApp(root);
-                        }
-                        } else if (keuzeMenu.getValue().equals("Lijnstuk")) {
-                            new DriehoekApp(root);
-                        }
-                    }
-		        });
-        }
+            keuzeMenu.setVisible(false);
+            if (keuzeMenu.getValue() != null) {
+                if (keuzeMenu.getValue().equals("Cirkel")) {
+                    new CirkelApp(root);
+                } else if (keuzeMenu.getValue().equals("Rechthoek")) {
+                    new RechthoekApp(root);
+                } else if (keuzeMenu.getValue().equals("Lijnstuk")) {
+                    new LijnStukApp(root);
+                }
+            } else if (keuzeMenu.getValue().equals("Lijnstuk")) {
+                new DriehoekApp(root);
+            }
+        });
+    }
 }
+
