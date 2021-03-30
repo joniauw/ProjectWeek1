f250dc4db5d789479d9218284291abb0a4b48f32
Robz459 <78813513+Robz459@users.noreply.github.com>
2021-03-30 10:39:56 +0200

    Robin De Koninck - Aaron Zwings Story 5

---
+++ src/domain/LijnStuk.java
@@ -0,0 +1,43 @@
+package domain;
+
+public class LijnStuk {
+    private Punt startPunt;
+    private Punt eindPunt;
+
+    public LijnStuk(Punt startpunt, Punt eindpunt) {
+        setStartEnEindPunt(startpunt, eindpunt);
+    }
+
+    private void setStartEnEindPunt(Punt startpunt, Punt eindpunt){
+        if(startpunt == null || eindpunt == null){
+            throw new DomainException("Start of eindpunt mag niet null zijn");
+        }
+        if(startpunt == eindpunt){
+            throw new DomainException("Startpunt is gelijk aan eindpunt");
+        }
+        this.startPunt = startpunt;
+        this.eindPunt = eindpunt;
+    }
+
+    public Punt getStartPunt() {
+        return this.startPunt;
+    }
+
+    public Punt getEindPunt() {
+        return this.eindPunt;
+    }
+
+    public boolean equals(Object o){
+        if (!(o instanceof LijnStuk)) return false;
+        else {
+            LijnStuk lijnStuk = (LijnStuk) o;
+            return lijnStuk.getStartPunt().equals(this.startPunt) && lijnStuk.getEindPunt().equals(this.eindPunt) ||
+                    lijnStuk.getStartPunt().equals(this.eindPunt) && lijnStuk.getEindPunt().equals(this.startPunt);
+        }
+    }
+
+    public String toString(){
+        return "Lijn: startpunt: " + startPunt + " - eindpunt: " + eindPunt;
+    }
+
+}
