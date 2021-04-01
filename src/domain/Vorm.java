710a6d8e12ba92566e9997ed2ecb0e6d8f3ff658
joniauw <r0763628@ucll.be>
2021-04-01 10:47:19 +0200

    Vorm update

--- src/domain/Vorm.java
+++ src/domain/Vorm.java
@@ -1,6 +1,11 @@
 package domain;
 
+import javafx.scene.paint.Color;
+
 public abstract class Vorm {
+    private Color kleur;
+    private boolean zichtbaar;
+
     @Override
     public String toString() {
         return toText() + " - " + getOmhullende().toString();
@@ -9,4 +14,20 @@ public abstract class Vorm {
     public abstract Omhullende getOmhullende();
 
     public abstract String toText();
+
+    public boolean isZichtbaar() {
+        return zichtbaar;
+    }
+
+    public void setZichtbaar(boolean zichtbaar) {
+        this.zichtbaar = zichtbaar;
+    }
+
+    public Color getKleur() {
+        return kleur;
+    }
+
+    public void setKleur(Color kleur) {
+        this.kleur = kleur;
+    }
 }
