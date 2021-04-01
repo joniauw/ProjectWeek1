75160d2f25ec49cd0097b2bf2b57a7924ae83772
joniauw <r0763628@ucll.be>
2021-04-01 12:23:25 +0200

    ok I guess

--- src/db/WoordenLezer.java
+++ src/db/WoordenLezer.java
@@ -22,4 +22,8 @@ public class WoordenLezer {
     public WoordenLijst getGelezenLijst() {
         return gelezenLijst;
     }
+
+    public String lees() {
+        return gelezenLijst.getRandomWoord();
+    }
 }
